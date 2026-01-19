d6489d4a3b4cfb285c069fa5e5dd47ffa645c98a
ADNC0 <abraham.nacimba5565@utc.edu.ec>
2026-01-19 15:09:29 -0500

    agregando ultimo repository de multa

---
+++ lib/repositories/multa_repository.dart
@@ -0,0 +1,43 @@
+import '/models/multa_models.dart';
+import '/settings/database_connection.dart';
+
+class MultaRepository {
+  final tableName = "multa";
+  final database = DatabaseConnection();
+
+  // insertar multa
+  Future<int> create(MultaModels data) async {
+    final db = await database.db;
+    return await db.insert(tableName, data.toMap());
+  }
+
+  // editar multa
+  Future<int> edit(MultaModels data) async {
+    final db = await database.db;
+    return await db.update(
+      tableName,
+      data.toMap(),
+      where: 'id_multa = ?',
+      whereArgs: [data.id_multa],
+    );
+  }
+
+  // eliminar multa
+  Future<int> delete(int id_multa) async {
+    final db = await database.db;
+    return await db.delete(
+      tableName,
+      where: 'id_multa = ?',
+      whereArgs: [id_multa],
+    );
+  }
+
+  // listar multas
+  Future<List<MultaModels>> getAll() async {
+    final db = await database.db;
+    final response = await db.query(tableName);
+    return response
+        .map((e) => MultaModels.fromMap(e))
+        .toList();
+  }
+}
